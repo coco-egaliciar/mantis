f190094ffaa94037bb0f3091cb4f2509a2811176
coco-egaliciar <38989410+coco-egaliciar@users.noreply.github.com>
2021-04-08 04:24:18 -0500

    add email support

--- config_inc_addon.php
+++ config_inc_addon.php
@@ -1,14 +1,19 @@
 <?php
-$g_window_title = '1Title of your MantisBT instance';
+$g_hostname               = 'mariadb';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_db_table_plugin_prefix   = 'mantis';
+$g_db_table_suffix          = 'mantis';
+
+$g_window_title = 'Mantis Coconutt';
 $g_reauthentication_expiry = 60 * 60 * 3;
 $g_max_file_size = 5 * 1024 * 1024;
 $g_phpMailer_method = PHPMAILER_METHOD_SMTP;
-$g_smtp_host = 'yourhost.com';
-$g_smtp_username = '19e3456c5a502e1e43f90a71e723a783';
-$g_smtp_password = '2532fe7d65be3c1b2e56400df3+65645740';
+$g_smtp_host = 'in-v3.mailjet.com';
+$g_smtp_username = '19e3436c5a502e1e43190a73';
+$g_smtp_password = '2532fe7d65be3c1b3e568df5650';
 $g_smtp_port = 25;
 $g_crypto_master_salt='18923712jh31283e813d8328387ksjdjjasdya6s=';
-?>
-
-
-
